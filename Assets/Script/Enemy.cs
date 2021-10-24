79a49b53bc0eb722153061613d8a600f0ce5ebb9
Teresa Bing <tereasabing0513lk@gmail.com>
2021-10-24 13:29:32 -0400

    e

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float walkSpeed;
+    public bool canPatrol;
+
+    public Rigidbody2D rb;
     void Start()
     {
-        
+        canPatrol = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(canPatrol)
+        {
+            Patrol();
+        }
     }
+
+    void Patrol()
+    {
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        canPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        canPatrol = true;
+    }    
 }
