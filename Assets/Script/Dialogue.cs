b670919d5256cf696083c1200779592417438155
Teresa Bing <tereasabing0513lk@gmail.com>
2021-10-31 00:46:49 -0400

    l

--- Assets/Script/Dialogue.cs
+++ Assets/Script/Dialogue.cs
@@ -9,10 +9,39 @@ public class Dialogue : MonoBehaviour
     public string[] sentences;
     private int index;
     public float typingS;
+    int counter = 0;
+    
 
     private void Start()
     {
+        textDisplay.text = "";
+        
+    }
+    private void OnEnable()
+    {
         StartCoroutine(Type());
+    }
+    private void Update()
+    {
+        //Vector2 right = new Vector2(736, transform.position.y);
+        //Vector2 left = new Vector2(420, transform.position.y);
+        //float x = transform.position.x;
+        counter++;
+        if (counter == 200)
+        {
+            /*
+            if(x == 420)
+            {
+                textDisplay.transform.position = right;
+            }
+            else if (x == 736)
+            {
+
+                textDisplay.transform.position = left;
+            }*/
+            Next();
+            counter = 0;
+        }
     }
     IEnumerator Type()
     {
@@ -23,4 +52,19 @@ public class Dialogue : MonoBehaviour
         }
     }
 
+    private void Next()
+    {
+        if(index<sentences.Length -1)
+        {
+            //auto play
+            index++;
+            //reset line
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+    }
 }
